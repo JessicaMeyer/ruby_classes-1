9bc4619c0173adb511b3dc6fdd8b7ff6cc92b65b
Jessica Meyer <JessLynnMeyer@gmail.com>
2014-05-29 11:00:35 -0500

    finished car exercise

--- car.rb
+++ car.rb
@@ -16,10 +16,17 @@ end
 			Kernel.puts "You might want to fill up soon!"
 		end
 		@distance=miles_driven
-		
+
+		# Part of exercise before adding the if/else statement
 		# @distance=miles_driven
 		# @fuel=20
 		# Kernel.puts("I'm a car! I have now driven #{@distance} miles and have #{@fuel} left in my tank.")
+end
+	def fuel_up(gallons_needed)
+		@fuel_up=gallons_needed
+		@fuel=30
+		gallons_needed = @fuel-20
+		Kernel.puts "You will need to pay $#{3.5*gallons_needed} to fill up your tank."
 end
 end
 
@@ -31,7 +38,17 @@ car_a.drive(10)
 car_b.drive(20)
 car_b.drive(105)
 car_b.drive(200)
+car_a.fuel_up(50)
 
-
-
+#Output 
+# 2.1.0 :004 > load 'car.rb'
+# the initialize method is running automatically
+# the initialize method is running automatically
+# the initialize method is running automatically
+# You're fine on gas for now!
+# You're fine on gas for now!
+# You might want to fill up soon!
+# You might want to fill up soon!
+#You will need to pay $35.0 to fill up your tank.
+#  => true 
 
